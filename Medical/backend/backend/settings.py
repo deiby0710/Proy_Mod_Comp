7b1b02301fa80933320c787e90a7193435fb4794
Santiago <iemccp7.5enriquezdavid@gmail.com>
2026-05-05 22:11:36 -0500

    last functional

--- Medical/backend/backend/settings.py
+++ Medical/backend/backend/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST', 'db'),  # contenedor de postgres
         'PORT': os.environ.get('DB_PORT', '5432'),
-        'CONN_MAX_AGE': 60,  # 🔥 mejora rendimiento (pooling básico)
+        'CONN_MAX_AGE': 60,
         'OPTIONS': {
             'connect_timeout': 5,
         }
@@ -114,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CO'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
